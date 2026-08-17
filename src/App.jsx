6a971cdab66a27d08b63df0351ea9aaa3c1f7b37
kkanal333-cell <kkanal333@gmail.com>
2026-08-17 17:20:18 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2511,16 +2511,16 @@ export default function App() {
                     <table className="w-full text-left border-collapse table-fixed min-w-[780px]">
                       <thead>
                         <tr className="border-b border-slate-200 text-slate-700 text-xs md:text-sm bg-slate-100 font-bold">
-                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '4px' }}>픽업일시</th>
-                          <th className="px-0.5 w-13" style={{ paddingTop: '3px', paddingBottom: '4px' }}>고객명</th>
-                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '4px' }}>연락처</th>
-                          <th className="px-0.5 w-14" style={{ paddingTop: '3px', paddingBottom: '4px' }}>상품명</th>
-                          <th className="px-0.5 w-16" style={{ paddingTop: '3px', paddingBottom: '4px' }}>금액</th>
-                          <th className="px-0.5 w-16" style={{ paddingTop: '3px', paddingBottom: '4px' }}>결제수단</th>
-                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '4px' }}>메모</th>
-                          <th className="px-0.5 w-20" style={{ paddingTop: '3px', paddingBottom: '4px' }}>접수일시</th>
+                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '3px' }}>픽업일시</th>
+                          <th className="px-0.5 w-14" style={{ paddingTop: '3px', paddingBottom: '3px' }}>고객명</th>
+                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '3px' }}>연락처</th>
+                          <th className="px-0.5 w-14" style={{ paddingTop: '3px', paddingBottom: '3px' }}>상품명</th>
+                          <th className="px-0.5 w-16" style={{ paddingTop: '3px', paddingBottom: '3px' }}>금액</th>
+                          <th className="px-0.5 w-16" style={{ paddingTop: '3px', paddingBottom: '3px' }}>결제수단</th>
+                          <th className="px-0.5 w-24" style={{ paddingTop: '3px', paddingBottom: '3px' }}>메모</th>
+                          <th className="px-0.5 w-22" style={{ paddingTop: '3px', paddingBottom: '3px' }}>접수일시</th>
                           <th className="px-0.5 w-20 text-center" style={{ paddingTop: '3px', paddingBottom: '3px' }}>관리 / 출력</th>
-                          <th className="px-0.5 w-15 text-center" style={{ paddingTop: '3px', paddingBottom: '3px' }}>사진</th>
+                          <th className="px-0.5 w-12 text-center" style={{ paddingTop: '3px', paddingBottom: '3px' }}>사진</th>
                           <th className="px-0.5 w-8 text-center" style={{ paddingTop: '3px', paddingBottom: '3px' }}>
                             <input
                               type="checkbox"
